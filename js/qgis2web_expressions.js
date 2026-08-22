c3d84cf241df2a88ed76b768c56335864acc8092
Fidelis Leonard Chidubem <leonfidelis5@gmail.com>
2026-08-22 15:40:03 +0100

    Add files via upload

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,3 +754,108 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
+
+
+
+function exp_label_FastChargers_5_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_48RackStation_6_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_24RacksStation_7_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_PetrolStationsBurgerKings_8_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_FastChargers_5_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_48RackStation_6_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_24RacksStation_7_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
+
+
+function exp_label_PetrolStationsBurgerKings_8_eval_expression(context) {
+    // Location || ' --- ' || "SS Size"
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Location']  + ' --- ') + feature.properties['SS Size'] );
+    } else {
+        return ((feature['Location']  + ' --- ') + feature['SS Size'] );
+    }
+}
